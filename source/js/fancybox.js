be34ba6d31a870954acf869d90b06b1b7687a307
JEB-Bem <jeb_alex@outlook.com>
2026-07-10 20:34:07 +0800

    perf: load theme assets conditionally

--- source/js/fancybox.js
+++ source/js/fancybox.js
@@ -1,26 +1,34 @@
 // fancybox js
-let fancyTimer = setInterval(function(){
-  if(!window.$){
-    return;
-  }
-  $(document).ready(function() {
-    $(".markdown-body img").each(function () {
-      if($(this).parent().get(0).tagName.toLowerCase() === "a") {
+(function () {
+  function initFancybox() {
+    if (!window.jQuery) {
+      return;
+    }
+
+    var $ = window.jQuery;
+    $(".post-content .markdown-body img").each(function () {
+      var parent = $(this).parent().get(0);
+      if (parent && parent.tagName.toLowerCase() === "a") {
         return;
       }
-      // $(this).attr("data-fancybox", "gallery"); // if you add 'data-fancybox', img will display after showed
+
       var element = document.createElement("a");
       $(element).attr("data-fancybox", "gallery");
       $(element).attr("style", "text-decoration: none; outline: none; border: 0px none transparent;");
-      // 判断是否启用了lazyload图片懒加载
+
       if ($(this).attr("data-original")) {
         $(element).attr("href", $(this).attr("data-original"));
       } else {
         $(element).attr("href", $(this).attr("src"));
       }
+
       $(this).wrap(element);
     });
+  }
 
-    clearInterval(fancyTimer);
-  });
-}, 10);
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", initFancybox, { once: true });
+  } else {
+    initFancybox();
+  }
+})();
